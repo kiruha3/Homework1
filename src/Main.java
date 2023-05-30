31d1b3a2ecbde83269bf4e683a67cdb5afe15a67
kirill <kiruha3@mail.ru>
2023-05-30 20:36:46 +0700

    Домашняя работа 2 - задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog -> " + dog);
+        System.out.println("cat -> " + cat);
+        System.out.println("paper -> " + paper);
     }
 
-    public static void task2 () {
-        System.out.println("Задача 2");
-    }
 }
