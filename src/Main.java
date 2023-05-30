e98ae00101bd4c3513708769d23c42dc9f1d8e11
kirill <kiruha3@mail.ru>
2023-05-30 22:37:44 +0700

    Домашняя работа 2 - задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
        //task1();
-        task2();
+        task3();
     }
 
     public static void task1 () {
@@ -21,10 +21,36 @@ public class Main {
         System.out.println("dog -> " + dog);
         System.out.println("cat -> " + cat);
         System.out.println("paper -> " + paper);
+
         dog = dog + 4;
         cat = cat + 4 ;
         paper = paper + 4;
         System.out.println("change paranmetrs + 4");
+        System.out.println("dog -> " + dog);
+        System.out.println("cat -> " + cat);
+        System.out.println("paper -> " + paper);
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog -> " + dog);
+        System.out.println("cat -> " + cat);
+        System.out.println("paper -> " + paper);
+
+        dog = dog + 4;
+        cat = cat + 4 ;
+        paper = paper + 4;
+        System.out.println("change paranmetrs + 4");
+        System.out.println("dog -> " + dog);
+        System.out.println("cat -> " + cat);
+        System.out.println("paper -> " + paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6 ;
+        paper = paper - 7639;
+        System.out.println("change paranmetrs: - ");
         System.out.println("dog -> " + dog);
         System.out.println("cat -> " + cat);
         System.out.println("paper -> " + paper);
