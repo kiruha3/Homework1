c5cea26cfe7b669255d96393b49cfcd7b6c6d129
kirill <kiruha3@mail.ru>
2023-06-03 15:46:31 +0700

    Домашняя работа 2 - 1 Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,120 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
-        task3();
-        task4();
-        task5();
-        task6();
-        task7();
-        task8();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-    }
-    public static void task2 () {
-        System.out.println("Задача 2");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-
-        dog = dog + 4;
-        cat = cat + 4 ;
-        paper = paper + 4;
-        System.out.println("change paranmetrs + 4");
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-    }
-    public static void task3 () {
-        System.out.println("Задача 3");
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-
-        dog = dog + 4;
-        cat = cat + 4 ;
-        paper = paper + 4;
-        System.out.println("change paranmetrs + 4");
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-
-        dog = dog - 3.5;
-        cat = cat - 1.6 ;
-        paper = paper - 7639;
-        System.out.println("change paranmetrs: - ");
-        System.out.println("dog -> " + dog);
-        System.out.println("cat -> " + cat);
-        System.out.println("paper -> " + paper);
-    }
-    public static void task4 () {
-        System.out.println("Задача 4");
-        var friend = 19;
-        System.out.println("friend_1 -> " + friend);
-        friend = friend + 2 ;
-        System.out.println("friend_2 -> " + friend);
-        friend = friend / 7 ;
-        System.out.println("friend_3 -> " + friend);
-    }
-
-    public static void task5 () {
-        System.out.println("Задача 5");
-        var frog = 3.5;
-        System.out.println("frog_1 -> " + frog);
-        frog = frog * 10 ;
-        System.out.println("frog_2 -> " + frog);
-        frog = frog / 3.5 ;
-        System.out.println("frog_3 -> " + frog);
-        frog = frog + 4 ;
-        System.out.println("frog_4 -> " + frog);
-    }
-    public static void task6 () {
-        System.out.println("Задача 6");
-        var weightFirstFiter = 78.2;
-        var weightSecondFiter = 82.7;
-        var sumTwoFiters = weightFirstFiter + weightSecondFiter;
-        var differenceFiters = weightSecondFiter - weightFirstFiter;
-        System.out.println("Общий вес 2 бойцов -> " + sumTwoFiters);
-        System.out.println("Разница в весе бойцов -> " + differenceFiters);
+        int firstInt = 1;
+        byte firstByte = 1;
+        short firstShort = 1;
+        long firstLong = 1;
+        float firstFloat = 1.0f;
+        double firstDouble = 1.0;
+        System.out.println("Значение переменной firstInt с типом int равно "+ firstInt);
+        System.out.println("Значение переменной firstByte с типом byte равно "+ firstByte);
+        System.out.println("Значение переменной firstShort  с типом short равно "+ firstShort);
+        System.out.println("Значение переменной firstLong с типом long равно "+ firstLong);
+        System.out.println("Значение переменной firstFloat  с типом float равно "+ firstFloat);
+        System.out.println("Значение переменной firstDouble с типом double равно "+ firstDouble);
 
     }
-    public static void task7 () {
-        System.out.println("Задача 7");
-        var weightFirstFiter = 78.2;
-        var weightSecondFiter = 82.7;
-        var lowToBig = weightSecondFiter - weightFirstFiter;
-        var modFiters = weightSecondFiter % weightFirstFiter;
-        System.out.println("Вычитание из большего веса меньшего -> " + lowToBig);
-        System.out.println("С помощь функции остаток от деления -> " + modFiters);
-    }
-    public static void task8 () {
-        System.out.println("Задача 8.1");
-        var hours = 640;
-        var hoursPerson = 8;
-        var countEmploer = hours / hoursPerson ;
-        System.out.println("Рабочих часов на всех сотрудников -> " + hours);
-        System.out.println("Рабочих часов на сотрудника -> " + hoursPerson);
-        System.out.println("Всего работников в компании — " + countEmploer +" человек");
-        System.out.println("Задача 8.2");
-        var moreCountEmploer = countEmploer +94;
-        System.out.println("Если работников в компании — " + moreCountEmploer +" человек");
-        var hoursPersonMore = hours / moreCountEmploer;
-        System.out.println("Если в компании работает "+ moreCountEmploer +" человек, то всего "
-                +  hoursPersonMore +" часов работы может быть поделено между сотрудниками");
 
-    }
 }
